ed01ce1c96bf0952b819ec6267c2e942f7a0f390
Chiciuc Nicușor <nicuchiciuc@gmail.com>
2020-02-26 15:21:47 +0200

    Update to typescript@next

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
-function main() {
-  const arr: Array<string> = [];
+const arr: Array<string> = [];
 
-  const [first] = arr;
-  const zero = arr[0];
+const [first] = arr;
+const zero = arr[0];
 
-  const str1: string = first;
-  const str2: string = zero;
-}
+const str1: string = first;
+const str2: string = zero;
